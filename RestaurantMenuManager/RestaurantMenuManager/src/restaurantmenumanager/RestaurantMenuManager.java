1c47b7c8831bc3dd36316b80def159cd279ea706
Tanya Ocampo <91620236+Tocampo627@users.noreply.github.com>
2022-06-21 11:03:09 -0500

    Add files via upload

---
+++ RestaurantMenuManager/RestaurantMenuManager/src/restaurantmenumanager/RestaurantMenuManager.java
@@ -0,0 +1,14 @@
+
+package restaurantmenumanager;
+import MenuUpdaterAppView.MenuManagerGUI;
+
+public class RestaurantMenuManager {
+
+ 
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new  MenuManagerGUI();
+    }
+    
+}
